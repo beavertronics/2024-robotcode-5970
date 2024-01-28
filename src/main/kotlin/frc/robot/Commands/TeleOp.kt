a25a7caf1a6573508b6d51d89fa29e05b5ea57ed
Ozy <ozypfl@gmail.com>
2024-01-27 18:24:23 -0800

    VERY basic intake code

--- src/main/kotlin/frc/robot/Commands/TeleOp.kt
+++ src/main/kotlin/frc/robot/Commands/TeleOp.kt
@@ -4,8 +4,10 @@ import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj2.command.Command
 import frc.engine.utils.Sugar.within
+import frc.robot.Constants
 
 import frc.robot.Constants.TeleopConstants
+import frc.robot.subsystems.Intake
 import kotlin.math.*
 
 import frc.robot.subsytems.Drivetrain
@@ -23,6 +25,11 @@ object TeleOp : Command() {
 
     override fun execute() {
         Drivetrain.rawCurvatureDrive(OI.throttle, OI.turn)
+        when {
+            OI.intake -> Intake.runIntakeRaw(Constants.IntakeConstants.voltage)
+            OI.outtake -> Intake.runIntakeRaw(-Constants.IntakeConstants.voltage)
+            else -> Intake.stop()
+        }
     }
 
     object OI {
@@ -45,7 +52,8 @@ object TeleOp : Command() {
 
         public val turn get() = driverController.leftX.processInput(squared = true)
         public val throttle get() = driverController.leftY.processInput(squared = true)
-
+        public val intake get() = operatorController.getRawButton(1) //Todo: Get right button
+        public val outtake get() = operatorController.getRawButton(2) //Todo: Get right button
         //TODO: Bring back this code- quickturns!
         //val quickTurnRight    get() = driverController.rightTriggerAxis
         //val quickTurnLeft     get() = driverController.leftTriggerAxis
