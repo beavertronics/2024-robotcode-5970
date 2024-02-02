14098f79e753a5050fbdca988bc32d744621aed9
Ozy King <ozypfl@gmail.com>
2024-02-01 16:17:45 -0800

    Updated Vision for PhotonVision

--- src/main/kotlin/frc/robot/Constants.kt
+++ src/main/kotlin/frc/robot/Constants.kt
@@ -1,9 +1,11 @@
 package frc.robot
 
+import edu.wpi.first.math.MatBuilder
+import edu.wpi.first.math.Matrix
+import edu.wpi.first.math.Nat
 import frc.engine.utils.Inches
 import frc.engine.utils.Meters
-import frc.engine.utils.Polynomial
-
+import edu.wpi.first.math.Num
 /* Constants for tuning the robot code.
 
 This should NOT include hardware constants (ie CAN bus IDs, wheel diameter and gear ratios); 
@@ -51,33 +53,9 @@ object Constants {
            For info on battery characteristics
          */
     }
-    object ShooterConstants {
-        const val leftFlywheel   = 5 //TODO
-        const val rightFlywheel = 6 //TODO
-        /** Proportional to the error (if it's bad, fix it. If it's really bad, fix it harder based on how bad it is)*/
-        const val KP            = 1.0 // tune later
-
-        /** Proportional to the slope (derivative) of the error
-         * (If it's OK but it's starting to go bad, fix it ahead of time, and if it's bad but it's
-         * getting close to being good, fix it less hard so it doesn't overshoot)*/
-        const val KD            = 0.0 // tune later
-
-        /** Propotional to the integral of the error
-         * (If it's been bad for a long time, fix it harder.)
-         * Usually very unstable; much safer to leave at 0.0 if possible
-         */
-        const val KI            = 0.0
-
-        /** (roughly) how much voltage to overcome static friction */
-        const val KS            = 0.0
-        /** How much voltage to maintain a velocity*/
-        const val KV            = 0.0
-        /** How much voltage to accelerate- Can go unused (0) */
-        const val KA            = 0.0
-
-        const val CurrentLimit = 20 //amps, per motor controller
-        val SpeakerPoly = Polynomial() //TODO Desmos this
-
+    object OdometryConstants {
+        @Suppress("removal")
+        val VisionDeviation = MatBuilder(Nat.N3(),Nat.N1()).fill(3.0,3.0,1000.0)
     }
 
     object TeleopConstants {
