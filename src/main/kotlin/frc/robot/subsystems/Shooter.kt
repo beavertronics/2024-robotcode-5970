319617a7a5ba36a134883350c541197382fcb2af
Ozy King <ozypfl@gmail.com>
2024-02-01 17:10:14 -0800

    Switching branches...

--- src/main/kotlin/frc/robot/subsystems/Shooter.kt
+++ src/main/kotlin/frc/robot/subsystems/Shooter.kt
@@ -1,7 +1,17 @@
 package frc.robot.subsystems
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.engine.utils.RPM
 
 object Shooter : SubsystemBase() {
+    /**
+     * Set the speed of the flywheels using closed loop control
+     * @param speed Speed of the motor in RPM
+     */
+    fun setSpeed(speed : Double){
 
+    }
+
+
+    fun setSpeed(speed : RPM) { setSpeed((speed.rotationsPerMinute()))}
 }
