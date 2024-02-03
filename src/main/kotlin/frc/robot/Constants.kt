7e3a822a542f4b2f63b0ea56aa4961b0e76e4491
Ozy King <ozypfl@gmail.com>
2024-02-03 13:29:20 -0800

    Polynomial Shooter Control

--- src/main/kotlin/frc/robot/Constants.kt
+++ src/main/kotlin/frc/robot/Constants.kt
@@ -2,7 +2,7 @@ package frc.robot
 
 import frc.engine.utils.Inches
 import frc.engine.utils.Meters
-import java.util.Currency
+import frc.engine.utils.Polynomial
 
 /* Constants for tuning the robot code.
 
@@ -15,10 +15,10 @@ be located in RobotHardware.kt instead.
 object Constants {
     object DriveConstants {
 
-        const val MotorRMainID = 1 //TODO Get neos from electrical
-        const val MotorRSubID  = 2 // Should start from 21, as is tradition.
-        const val MotorLMainID = 3
-        const val MotorLSubID  = 4
+        const val MotorRMainID   = 1 //TODO Get neos from electrical
+        const val MotorRSubID   = 2 // Should start from 21, as is tradition.
+        const val MotorLMainID    = 3
+        const val MotorLSubID    = 4
     
         const val MotorRevsPerWheelRev = (50/14) * (48/16) //~10:1 As taken from CAD- should verify after drivetrain is built in the real world
         val WheelDiameter = Inches(6.0).meterValue()
@@ -31,6 +31,12 @@ object Constants {
          * (If it's OK but it's starting to go bad, fix it ahead of time, and if it's bad but it's 
          * getting close to being good, fix it less hard so it doesn't overshoot)*/
         const val KD            = 0.0 // tune later
+
+        /** Propotional to the integral of the error
+         * (If it's been bad for a long time, fix it harder.)
+         * Usually very unstable; much safer to leave at 0.0 if possible
+        */
+        const val KI            = 0.0
 
 
         /** (roughly) how much voltage to overcome static friction */
@@ -70,31 +76,15 @@ object Constants {
         const val KA            = 0.0
 
         const val CurrentLimit = 20 //amps, per motor controller
+        val SpeakerPoly = Polynomial() //TODO Desmos this
 
     }
 
     object TeleopConstants {
-        const val MaxVoltage = 5.0
-        const val driveSpeed = 0.7 //TODO set drive speed
-        const val speedBoostSpeed = 1.0 //TODO set speed boost speed
-        const val quickTurnDeadzone = 0.1 //TODO set quick turn dead zone
-        const val quickTurnSpeed = 0.5 //TODO set quick turn speed.
-        
-        //const val MaxSpeed = 5.0 //M/s
+        const val MaxSpeed = 5.0 //M/s
         //TODO: Maybe change to feet per second? Metric is Good, but many teams communicate drivetrain speed in feet per second so for communicating quickly it could be worth leaving in fps.
         //  Possible implementation:
         //    FeetPerSecond(16.5).metersPerSecondValue()
         //    (also import utils/Units.kt)
-    }
-
-    object IntakeConstants {
-        const val CurrentLimit = 12
-        const val voltage = 1.0
-        const val limitSwitchChannel = 1 // TODO get limit switch channel number
-        const val intakeSpeed = 0.5 //TODO set intake speed.
-        const val pullbackSpeed = 0.3 //TODO set pullback speed. Multiplied by -1
-        const val feedingSpeed = 1.0 //TODO set feeding speed.
-        const val feedingTime = 1.0 //TODO set feedingTime. In seconds
-        const val outtakeSpeed = 0.6 //TODO set outtake Speed.
     }
 }
