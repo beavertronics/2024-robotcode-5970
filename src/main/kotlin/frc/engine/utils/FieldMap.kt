09315da758a719128968a570ed2b818fc0d3e901
Ozy King <ozypfl@gmail.com>
2024-02-03 14:41:10 -0800

    Added FieldMap

---
+++ src/main/kotlin/frc/engine/utils/FieldMap.kt
@@ -0,0 +1,26 @@
+package frc.engine.utils
+
+import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.math.geometry.Pose2d
+import frc.engine.utils.geometry.Coordinate
+import frc.engine.utils.geometry.Line
+import frc.engine.utils.geometry.Polygon
+import frc.engine.utils.geometry.Rectangle
+/*val blueMap = Map(
+    Speaker(
+
+    )
+)
+class Speaker (
+    Center: Coordinate,
+    Opening: Rectangle
+)
+
+
+
+class Map (
+    val speaker : Speaker
+) {
+
+}*/
