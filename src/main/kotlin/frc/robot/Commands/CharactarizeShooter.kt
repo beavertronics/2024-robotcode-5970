b12562a4ffd725d7599e1a29047d90b8f272f9e1
Ozy King <ozypfl@gmail.com>
2024-02-18 10:49:52 -0800

    Shooter stuff

---
+++ src/main/kotlin/frc/robot/Commands/CharactarizeShooter.kt
@@ -0,0 +1,41 @@
+package frc.robot.Commands
+
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine
+import frc.robot.subsystems.Shooter
+
+var routine = SysIdRoutine(
+    SysIdRoutine.Config(),
+    SysIdRoutine.Mechanism(
+        Shooter.rawDrive,
+        logger,
+        Shooter
+    )
+)
+fun sysIdQuasistatic(direction: SysIdRoutine.Direction): Command? {
+    return routine.quasistatic(direction)
+}
+
+fun sysIdDynamic(direction: SysIdRoutine.Direction): Command? {
+    return routine.dynamic(direction)
+}
+
+class ID_THAT_SYS : Command() {
+    private lateinit var autoCommandGroup: Command
+
+    override fun initialize() {
+        autoCommandGroup = SequentialCommandGroup (
+            sysIdQuasistatic(SysIdRoutine.Direction.kForward),
+            sysIdQuasistatic(SysIdRoutine.Direction.kReverse),
+            sysIdDynamic(SysIdRoutine.Direction.kForward),
+            sysIdDynamic(SysIdRoutine.Direction.kReverse)
+        )
+        autoCommandGroup.schedule()
+    }
+
+    override fun isFinished(): Boolean {
+        return autoCommandGroup.isFinished
+    }
+
+}
